849a8bab1a6b3ef4b7da56422f710ef7abf27ab4
Emam <emamAhmed36@gmail.com>
2019-10-14 10:17:45 +0200

    handle live data count case

--- app/src/main/java/com/example/viewmodelandlivedata/MainActivity.kt
+++ app/src/main/java/com/example/viewmodelandlivedata/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.viewmodelandlivedata
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -9,15 +10,22 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var counterViewModel: CounterViewModel
 
-
+    private lateinit var counterLiveData: CounterLiveData
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        counterViewModel = ViewModelProviders.of(this).get(CounterViewModel::class.java)
-        setNumber(counterViewModel.count)
+//        counterViewModel = ViewModelProviders.of(this).get(CounterViewModel::class.java)
+
+        counterLiveData = ViewModelProviders.of(this).get(CounterLiveData::class.java)
+        counterLiveData.counter.observe(this, Observer { conut ->
+            setNumber(conut)
+        })
+//        setNumber(counterViewModel.count)
+
         btnCount.setOnClickListener {
-            counterViewModel.count = counterViewModel.count + 1
-            setNumber(counterViewModel.count)
+            //            counterViewModel.count = counterViewModel.count + 1
+//            setNumber(counterViewModel.count)
+            counterLiveData.counter.value = counterLiveData.counter.value?.plus(1)
         }
     }
 
